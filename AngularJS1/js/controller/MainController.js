dd0a38b5d51c2227fe41d771e619989e9e1d2805
Gustavo Magalhães <itsgustavopereira@gmail.com>
2017-02-10 23:23:31 -0300

    commit

--- AngularJS1/js/controller/MainController.js
+++ AngularJS1/js/controller/MainController.js
@@ -13,7 +13,7 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'The Book of Trees', 
       price: 19, 
       pubdate: new Date('2014', '03', '08'), 
-      cover: 'img/the-book-of-trees.jpg',
+//      cover: 'img/the-book-of-trees.jpg',
       likes: 0,
       dislikes: 0
     }, 
@@ -21,7 +21,7 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Program or be Programmed', 
       price: 8, 
       pubdate: new Date('2013', '08', '01'), 
-      cover: 'img/program-or-be-programmed.jpg',
+//      cover: 'img/program-or-be-programmed.jpg',
       likes: 0,
       dislikes: 0 
     },
@@ -29,7 +29,7 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Coxinha', 
       price: 39, 
       pubdate: new Date('2014', '03', '08'), 
-      cover: 'img/the-book-of-trees.jpg',
+//      cover: 'img/the-book-of-trees.jpg',
       likes: 0,
       dislikes: 0 
     }, 
@@ -37,7 +37,7 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'FAIL', 
       price: 15, 
       pubdate: new Date('2013', '08', '01'), 
-      cover: 'img/program-or-be-programmed.jpg',
+//      cover: 'img/program-or-be-programmed.jpg',
       likes: 0,
       dislikes: 0 
     }
